e8aff4112c669c42389b7a9f437e98fd6cd6be2a
harrypynn <harry@harrypynn.com>
2018-02-28 22:40:38 +0000

    Added time picker for selecting the time of the first start.

--- startsequence.js
+++ startsequence.js
@@ -1,46 +1,66 @@
 var starts = [
     {
-        name: "Lark and RS 200", 
+        name: "Lark and RS 200",
         flag: "Lark Insignia"
     },
     {
-        name: "Cadet", 
+        name: "Cadet",
         flag: "Y"
     },
     {
-        name: "Dragonfly", 
+        name: "Dragonfly",
         flag: "W"
     },
     {
-        name: "Wayfarer", 
+        name: "Wayfarer",
         flag: "Wayfarer Insignia"
     },
     {
-        name: "Laser", 
+        name: "Laser",
         flag: "Laser Insignia"
     },
     {
-        name: "Squib", 
+        name: "Squib",
         flag: "Nav 9"
     }
 ];
-var now = new Date();
-var startTime = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 13, 20, 0, 0);
 function formatTime(t) {
-    return t.getHours() + ":" + t.getMinutes()
+    var minutes = "0" + t.getMinutes();
+    return t.getHours() + ":" + minutes.substr(-2);
 }
 function addMinutes(t, minutes) {
     t.setMinutes(t.getMinutes() + minutes);
 }
 $(document).ready(function () {
-    var flags = [];
+    var picker = new Picker(document.querySelector('.js-time-picker'), {
+        format: 'HH:mm',
+    });
+    var now = new Date();
+    var startTime = picker.getDate();
+    showStartSequence(startTime);
+    $('.js-time-picker').on('pick', function(p) {
+        var pickedDate = p.target.picker.getDate();
+        showStartSequence(pickedDate);
+    });
+});
 
+function showStartSequence(startTime) {
+    $("#starts").empty().append(
+        $('<tr/>').append(
+            $('<th/>').text('Event'),
+            $('<th/>').text('Time'),
+            $('<th/>').text('Up'),
+            $('<th/>').text('Down')
+        )
+    );
+
+    var flags = [];
     var flagTime = startTime;
     addMinutes(flagTime, -3);
     $.each(starts, function (i, el) {
         var lastStart = flags[flags.length - 1];
         if (lastStart) {
-            lastStart.name = lastStart.name +  ", " +  el.name + " 3 minute";
+            lastStart.name = lastStart.name + ", " + el.name + " 3 minute";
             lastStart.up = el.flag + ', P'
         }
         else {
@@ -48,7 +68,7 @@ $(document).ready(function () {
                 name: el.name + " 3 minute",
                 time: formatTime(flagTime),
                 up: el.flag + ', P'
-            });    
+            });
         }
         addMinutes(flagTime, 2);
         flags.push({
@@ -58,12 +78,12 @@ $(document).ready(function () {
         });
         addMinutes(flagTime, 1);
         flags.push({
-            name: el.name  + " Start",
+            name: el.name + " Start",
             time: formatTime(flagTime),
             down: el.flag
         });
     });
-    $.each(flags, function (i,  el) {
+    $.each(flags, function (i, el) {
         $("#starts").append(
             $("<tr/>").append(
                 $("<td/>").text(
@@ -81,4 +101,4 @@ $(document).ready(function () {
             )
         );
     });
-});
+}
